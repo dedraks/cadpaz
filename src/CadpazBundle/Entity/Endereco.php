95832e14c19959d94090eb75a1a93022b25d1a73
Carlos Alberto Borges Garcia Jr <carlos@in4byte.com>
2015-08-26 14:45:16 -0300

    Entidade Endereco criada.

---
+++ src/CadpazBundle/Entity/Endereco.php
@@ -0,0 +1,334 @@
+<?php
+
+namespace CadpazBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Endereco
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Endereco
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=50)
+     */
+    private $nome;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="padrao", type="boolean")
+     */
+    private $padrao;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="logradouro", type="string", length=100)
+     */
+    private $logradouro;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero", type="string", length=20)
+     */
+    private $numero;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="complemento", type="string", length=50)
+     */
+    private $complemento;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="bairro", type="string", length=100)
+     */
+    private $bairro;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="municipio", type="string", length=100)
+     */
+    private $municipio;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cep", type="string", length=2)
+     */
+    private $cep;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uf", type="string", length=10)
+     */
+    private $uf;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="obs", type="text")
+     */
+    private $obs;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     * @return Endereco
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string 
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set padrao
+     *
+     * @param boolean $padrao
+     * @return Endereco
+     */
+    public function setPadrao($padrao)
+    {
+        $this->padrao = $padrao;
+    
+        return $this;
+    }
+
+    /**
+     * Get padrao
+     *
+     * @return boolean 
+     */
+    public function getPadrao()
+    {
+        return $this->padrao;
+    }
+
+    /**
+     * Set logradouro
+     *
+     * @param string $logradouro
+     * @return Endereco
+     */
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+    
+        return $this;
+    }
+
+    /**
+     * Get logradouro
+     *
+     * @return string 
+     */
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param string $numero
+     * @return Endereco
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return string 
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set complemento
+     *
+     * @param string $complemento
+     * @return Endereco
+     */
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+    
+        return $this;
+    }
+
+    /**
+     * Get complemento
+     *
+     * @return string 
+     */
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    /**
+     * Set bairro
+     *
+     * @param string $bairro
+     * @return Endereco
+     */
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+    
+        return $this;
+    }
+
+    /**
+     * Get bairro
+     *
+     * @return string 
+     */
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * Set municipio
+     *
+     * @param string $municipio
+     * @return Endereco
+     */
+    public function setMunicipio($municipio)
+    {
+        $this->municipio = $municipio;
+    
+        return $this;
+    }
+
+    /**
+     * Get municipio
+     *
+     * @return string 
+     */
+    public function getMunicipio()
+    {
+        return $this->municipio;
+    }
+
+    /**
+     * Set cep
+     *
+     * @param string $cep
+     * @return Endereco
+     */
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+    
+        return $this;
+    }
+
+    /**
+     * Get cep
+     *
+     * @return string 
+     */
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    /**
+     * Set uf
+     *
+     * @param string $uf
+     * @return Endereco
+     */
+    public function setUf($uf)
+    {
+        $this->uf = $uf;
+    
+        return $this;
+    }
+
+    /**
+     * Get uf
+     *
+     * @return string 
+     */
+    public function getUf()
+    {
+        return $this->uf;
+    }
+
+    /**
+     * Set obs
+     *
+     * @param string $obs
+     * @return Endereco
+     */
+    public function setObs($obs)
+    {
+        $this->obs = $obs;
+    
+        return $this;
+    }
+
+    /**
+     * Get obs
+     *
+     * @return string 
+     */
+    public function getObs()
+    {
+        return $this->obs;
+    }
+}
