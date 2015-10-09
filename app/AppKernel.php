9a87eaca94c9d41c4c8f454620eae3eb9fed53cc
Carlos Alberto Borges Garcia Jr <dedraks@gmail.com>
2015-10-09 00:07:50 -0300

    CRUD de usuarios criado

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Ob\HighchartsBundle\ObHighchartsBundle(),
             new CadpazBundle\CadpazBundle(),
             new Dedraks\ConfigBundle\DedraksConfigBundle(),
+            new Dedraks\UserManagerBundle\DedraksUserManagerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Dedraks/UserManagerBundle/DedraksUserManagerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dedraks\UserManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DedraksUserManagerBundle extends Bundle
+{
+}
